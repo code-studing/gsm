b1cc2c23f1788fa9cccc1a896fdad9e15aa93440
egoing <egoing@gmail.com>
2018-07-14 12:08:15 +0900

    복습

--- day3.js
+++ day3.js
@@ -6,3 +6,15 @@ if(prompt('password?') === '111111'){
 } else {
 	alert('뉘슈?');
 }
+var a = ['a','b','c'];
+var i = 0;
+while(i < a.length){
+	document.write('<li>'+a[i]+'</li>');
+	//i = i + 1;
+	i++;
+}
+function sum(left, right){
+	return left+right;
+}
+alert(sum(1,5));
+email('a@a.com', sum(1,5))
