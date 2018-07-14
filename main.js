cb64df5833a95bb5891d32a8790e675d97cb1efe
egoing <egoing@gmail.com>
2018-07-14 15:18:17 +0900

    apply readFile

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
 var http = require('http');
 var url = require('url');
-
-//create a server object:
+var fs = require('fs');
 http.createServer(function (req, res) {
   var qs =url.parse(req.url, true).query;
-  console.log(qs.id);
-  res.write(`
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - HTML</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="index.html">WEB</a></h1>
-    <ol>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ol>
-    <h2>${qs.id}</h2>
-    <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-    <img src="coding.jpg" width="100%">
-    </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-    </p>
-  </body>
-  </html>
-  `); //write a response to the client
-  res.end(); //end the response
+
+  fs.readFile('./data/JavaScript', 'utf8', function(error, data){
+    res.write(`
+    <!doctype html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="index.html">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${qs.id}</h2>
+      <p>HTML is ...</p>
+    </body>
+    </html>
+    `); //write a response to the client
+    res.end(); //end the response
+  })
+
+
 }).listen(8080); //the server object listens on port 8080
